4ba4fc59aaa8afb676eddd7198202dbd35bb4d1d
Verisimilitude <acharyapiyush1@outlook.com>
2022-06-08 12:15:53 -0700

    Add files via upload

---
+++ Impending Doom.py
@@ -0,0 +1,65 @@
+"""
+LESSON: 6.3 - Complex Parameters
+Impending Doom
+"""
+
+#### ---- LIBRARIES ---- ####
+import random
+import tsk
+import pygame
+pygame.init()
+
+
+#### ------------------------------- ####
+#### ---- MOVE TOWARDS FUNCTION ---- ####
+#### ------------------------------- ####
+def move_towards(butterfly, vase, speed):
+    # Get start position
+    x = butterfly.center_x
+
+    # Calculate position
+    if vase.center_x < x - 2:
+        butterfly.flip_x = True
+        x -= speed
+    elif vase.center_x > x + 2:
+        butterfly.flip_x = False
+        x += speed
+        
+    # Move butterfly
+    butterfly.center_x = x
+    return
+
+#### ---------------------- ####
+#### ---- MAIN PROGRAM ---- ####
+#### ---------------------- ####
+w = pygame.display.set_mode([1018, 573])
+c = pygame.time.Clock()
+
+# Sprites
+background = tsk.Sprite("LivingRoom.jpg", 0, 0)
+vase = tsk.Sprite("TennisBall.png", 0, 200)
+bug = tsk.Sprite("Pug.png", 500, 250)
+
+#### ---- MAIN LOOP ---- ####
+drawing = True
+while drawing:
+
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            drawing = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            vase.x = random.choice([0, 848])
+
+    # Move butterfly
+    move_towards(bug, vase, .1 * c.get_time())
+
+    # Draw
+    background.draw()
+    vase.draw()
+    bug.draw()
+
+    # Finish
+    pygame.display.flip()
+    c.tick(30)
